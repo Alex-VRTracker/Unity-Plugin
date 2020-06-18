a8402024810ac1194a2dc61d219992ece368a83d
Alan <caillerezalan@gmail.com>
2020-06-18 12:26:08 +0200

    Add condition to read ip from file

--- Network/Scripts/VRT_NetworkAutoStart.cs
+++ Network/Scripts/VRT_NetworkAutoStart.cs
@@ -2,75 +2,93 @@
 using UnityEngine;
 using System.Globalization;
 using VRTracker.Manager;
+using Torrus.Settings;
 
 /**
  * VR Tracker - Network
  **/
 
-namespace VRTracker.Network {
-    
-	/// <summary>
-	/// VR Tracker network auto start
-	/// Tries to find a Server for 2 seconds using NetworkDiscovery
-	/// and starts a Host or Server if the server wasn't found 
-	/// </summary>
-	public class VRT_NetworkAutoStart : MonoBehaviour {
+namespace VRTracker.Network
+{
+
+    /// <summary>
+    /// VR Tracker network auto start
+    /// Tries to find a Server for 2 seconds using NetworkDiscovery
+    /// and starts a Host or Server if the server wasn't found 
+    /// </summary>
+    public class VRT_NetworkAutoStart : MonoBehaviour
+    {
 
         [Tooltip("Check to enable host mode")]
         public bool host = false; //Disabled by default on android devices
 
-		public VRTracker.Network.VRT_NetworkDiscovery networkDiscovery;
-		public VRTracker.Network.VRT_NetworkManager networkManager;
-		private bool hostFound = false;
+        public VRTracker.Network.VRT_NetworkDiscovery networkDiscovery;
+        public VRTracker.Network.VRT_NetworkManager networkManager;
+        private bool hostFound = false;
         private int timeOut = 10;
         private int currentConnectionTime = 0;
-		// Use this for initialization
-		void Start () {
-            currentConnectionTime = 0;
-            networkDiscovery = FindObjectOfType<VRTracker.Network.VRT_NetworkDiscovery>();
-			networkManager = FindObjectOfType<VRTracker.Network.VRT_NetworkManager>();
 
-            // Either we use the Network Discovery if enabled...
-            if (networkDiscovery != null && networkDiscovery.enabled) {
-                if (host && SpectatorManager.Instance.spectator)
+        [SerializeField]
+        private string serverIp = "192.168.1.113";
+
+        // Use this for initialization
+        void Start()
+        {
+            if (GetComponent<UnetServerIP>().GetServerIp(ref serverIp))
+            {
+                networkManager.JoinGame(serverIp);
+            }
+            else
+            {
+                currentConnectionTime = 0;
+                networkDiscovery = FindObjectOfType<VRTracker.Network.VRT_NetworkDiscovery>();
+                networkManager = FindObjectOfType<VRTracker.Network.VRT_NetworkManager>();
+
+                // Either we use the Network Discovery if enabled...
+                if (networkDiscovery != null && networkDiscovery.enabled)
                 {
-                    Debug.LogError("NETWORK: Cannot be host (VRT_NetworkAutoStart) and Spectator (VRT_Manager)");
+                    if (host && SpectatorManager.Instance.spectator)
+                    {
+                        Debug.LogError("NETWORK: Cannot be host (VRT_NetworkAutoStart) and Spectator (VRT_Manager)");
+                    }
+                    else if (SpectatorManager.Instance.spectator)
+                    {
+                        networkManager.StartLanServer();
+                        networkDiscovery.StartBroadcast();
+                    }
+                    else if (host)
+                    {
+                        networkManager.StartLanHost();
+                        networkDiscovery.StartBroadcast();
+                    }
+                    else
+                    {
+                        StartCoroutine(WaitForLanBoradcast());
+                    }
                 }
-                else if (SpectatorManager.Instance.spectator)
-                {
-                    networkManager.StartLanServer();
-                    networkDiscovery.StartBroadcast();
-                }
-                else if (host)
-                {
-                    networkManager.StartLanHost();
-                    networkDiscovery.StartBroadcast();
-                }
+
+                // ...Or we use the Gateway to get Server IP
                 else
                 {
-                    StartCoroutine(WaitForLanBoradcast());
-                }				
-			}
-
-            // ...Or we use the Gateway to get Server IP
-            else {
-                if(SpectatorManager.Instance.spectator && host){
-                    Debug.LogError("NETWORK: Cannot be host (VRT_NetworkAutoStart) and Spectator (VRT_Manager)");
-                }
-                else if (SpectatorManager.Instance.spectator)
-                {
-                    networkManager.StartLanServer();
-                }
-                else if (host)
-                {
-                    networkManager.StartLanHost();
-                }
-                else
-                {
-                    StartCoroutine(WaitForServerIP());
+                    if (SpectatorManager.Instance.spectator && host)
+                    {
+                        Debug.LogError("NETWORK: Cannot be host (VRT_NetworkAutoStart) and Spectator (VRT_Manager)");
+                    }
+                    else if (SpectatorManager.Instance.spectator)
+                    {
+                        networkManager.StartLanServer();
+                    }
+                    else if (host)
+                    {
+                        networkManager.StartLanHost();
+                    }
+                    else
+                    {
+                        StartCoroutine(WaitForServerIP());
+                    }
                 }
             }
-		}
+        }
 
 
         /// <summary>
@@ -78,13 +96,13 @@ namespace VRTracker.Network {
         /// </summary>
         /// <returns>The for lan boradcast.</returns>
 		IEnumerator WaitForLanBoradcast()
-		{
-            while(!hostFound && currentConnectionTime < timeOut)
+        {
+            while (!hostFound && currentConnectionTime < timeOut)
             {
                 yield return new WaitForSeconds(1);
                 currentConnectionTime++;
             }
-            if(currentConnectionTime >= timeOut)
+            if (currentConnectionTime >= timeOut)
             {
                 Debug.Log("No server found");
                 if (SpectatorManager.Instance.spectator)
@@ -99,7 +117,7 @@ namespace VRTracker.Network {
                 }
             }
             yield return null;
-		}
+        }
 
         /// <summary>
         /// Waits in loop to receive server IP from the Gateway before starting
@@ -122,12 +140,13 @@ namespace VRTracker.Network {
             yield return null;
         }
 
-		public void BrodcastReception(string ipAddress){
+        public void BrodcastReception(string ipAddress)
+        {
             //TODO: Check if this would work with two instance on the same PC
-			if (hostFound || ipAddress == "localhost")
-				return;
-			hostFound = true;
-			networkManager.JoinGame (ipAddress);
-		}
-	}
+            if (hostFound || ipAddress == "localhost")
+                return;
+            hostFound = true;
+            networkManager.JoinGame(ipAddress);
+        }
+    }
 }
